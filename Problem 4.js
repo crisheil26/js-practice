88a6ca0fbdfa6dbc92703e6b5cdae8587c3acb72
crisheil26 <altheacrisheild@gmail.com>
2025-03-21 22:02:31 +0800

    Problem 4.js

--- Problem 4.js
+++ Problem 4.js
@@ -1,16 +1,18 @@
-const favoriteNumber = 6;
-let userGuess = null;
+// Declare the favorite number
+let favoriteNumber = 6;  
 
-while (userGuess !== favoriteNumber) {
-    userGuess = parseInt(prompt("Guess my favorite number:"),10);
+// Initialize user guess
+let userGuess = prompt("Guess my favorite number:");
 
-    if (userGuess < favoriteNumber) {
-        console.log("Too low! Try again.");
-    } else if (userGuess > favoriteNumber) {
-        console.log("Too high! Try again.");
-    } else if (userGuess === favoriteNumber) {
-         console.log("Correct! You guessed my favorite number .");
+// Keep asking until the correct number is guessed
+while (userGuess != favoriteNumber) {
+    if (userGuess > favoriteNumber) {
+        userGuess = prompt("Too high! Try again:");
     } else {
-        console.log("Invalid input. Please enter a number .");
+        userGuess = prompt("Too low! Try again:");
     }
 }
+
+// Correct guess message
+console.log("Correct! My favorite number is 6.");
+
