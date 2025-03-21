7e4a8e58dce34c29645bedebcd9447fad12f6e6f
crisheil26 <altheacrisheild@gmail.com>
2025-03-21 21:21:10 +0800

    userProfile.js

--- userProfile.js
+++ userProfile.js
@@ -1,1 +1,11 @@
+// Collect user information
+let classInfo = {
+    subjectTitle: prompt("Enter Subject Title:"), // subject title
+    classSchedule: prompt("Enter Class Schedule (Time, Days):"), // class schedule
+    classroom: prompt("Enter Classroom:"), // classroom number
+    classInstructor: prompt("Enter Class Instructor:"), // instructor's name
+    studentName: prompt("Enter Student Name:") // student's name
+};
 
+// Log the collected information
+console.log(`${classInfo.studentName} is currently enrolled in ${classInfo.subjectTitle} with a class schedule of ${classInfo.classSchedule} at room ${classInfo.classroom}, instructed by ${classInfo.classInstructor}.`);
