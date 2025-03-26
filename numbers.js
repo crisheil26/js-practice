27eb3ea9cd7f7fa3294f7bc7fc82e6152736a9eb
crisheil26 <altheacrisheild@gmail.com>
2025-03-26 14:40:24 +0800

    numbers.js

---
+++ numbers.js
@@ -0,0 +1,27 @@
+// Declare favorite numbers
+const firstFavNum = 6;
+const secondFavNum = 26;
+
+//perform operations
+const sum = favnumber1 + favnumber2;
+console.log(sum);
+
+//perform operations
+const difference = favnumber1 - favnumber2;
+console.log(difference);
+
+//perform operations
+const product = favnumber1 * favnumber2;
+console.log(product);
+
+//perform operations
+const quotient = favnumber1 / favnumber2;
+console.log(quotient);
+
+//perform operations
+const reminder = favnumber1 % favnumber2;
+console.log(remainder);
+
+//perform operations
+const power = favnumber1 ** favnumber2;
+console.log(power);
