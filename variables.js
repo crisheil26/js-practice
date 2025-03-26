106fc51c1990df7bf32805d3edb6b3c4a5dba47f
crisheil26 <altheacrisheild@gmail.com>
2025-03-26 13:44:10 +0800

    variables.js

---
+++ variables.js
@@ -0,0 +1,16 @@
+// Declare variables
+let name = "Althea";
+// Log variables to the console
+console.log("name");
+// Declare variables
+let whatDoYouWannaBecomeInYourLife = "programmer";
+// Log variables to the console
+console.log(" whatDoYouWannaBecomeInYourLife");
+// Declare variables
+let gender = "female";
+// Log variables to the console
+console.log(" gender");
+// Declare variables
+let twitterHandle = "althea";
+// Log variables to the console
+console.log("twitterHandle");
